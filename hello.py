20fd8eefb67d683986d63fd54a9aa5c8513bf941
Jake Shankman <jes3cu@virginia.edu>
2015-09-28 16:56:35 -0400

    Moved greeing from hello.py to helper.py and imported in hello.py

--- hello.py
+++ hello.py
@@ -1,12 +1,11 @@
+__author__ = "jes3cu"
 # Name: Jake Shankman
 # CompID: jes3cu
 # Date: 9/23/15
 # Assignment: Lab 5 Pre-lab
 # Language: python3
 
-def greeting(msg):
-	print(msg)
-
+import helper
 
 if __name__ == "__main__":
-	greeting("hello")
+	helper.greeting("hello")
---
+++ helper.py
@@ -0,0 +1,13 @@
+__author__ = 'jes3cu'
+# Name: Jake Shankman
+# CompID: jes3cu
+# Date: 9/28/15
+# Assignment: Lab 5
+# Language: python3
+
+def greeting(msg):
+	print(msg)
+
+if __name__ == "__main__":
+	pass
+
